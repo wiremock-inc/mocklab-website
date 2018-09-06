e978c9b9822b91fa28ef714e44f2783715db4f5f
Tom Akehurst <t.m.akehurst@googlemail.com>
2018-09-06 11:30:55 +0100

    Updated

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ export ALGOLIA_API_KEY='61e953f848b8a1d05c3be4620a5286db'
 # rbenv exec bundle exec jekyll algolia push
 rbenv exec bundle exec jekyll build
 
-pushd ../mocklab-docs
+pushd ../mocklab-website
 git reset --hard origin/gh-pages
 git pull
 cp -rf ../mocklab-docs-sources/_site/* .
