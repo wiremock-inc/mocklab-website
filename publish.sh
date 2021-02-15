1836e895784078945f51d116c5641b7eb47e5bc4
Tom Akehurst <t.m.akehurst@gmail.com>
2021-02-15 18:26:01 +0000

    Updated

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 npm run raml
-export MIXPANEL_ID=71b55ac57b47107ca89d12dc7c75cf00
 export ALGOLIA_API_KEY='61e953f848b8a1d05c3be4620a5286db'
 # rbenv exec bundle exec jekyll algolia push
 rbenv exec bundle exec jekyll build
